7be72106d5f7fb52d4b0d110fe38d7276c4094b8
maribelcuales <maribelcuales@gmail.com>
2020-10-09 04:39:37 +0800

    Parse postgres url using pg-connection-string then passing in the connection string in production.

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 
-const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
-// if using a local postgres server, please create the database manually, Knex will not create it autmatically 
+// const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+// if using a local postgres server, please create the database manually, Knex will not create it autmatically
+
+const parse = require('pg-connection-string').parse;
+
+const configConnection = parse('postgres://postgres:secretpassword@localhost:5433/auth');
+
+const pgConnection = process.env.DATABASE_URL ||  configConnection;
 
 module.exports = {
   development: {
@@ -25,13 +31,13 @@ module.exports = {
 
   production: {
     client: "pg",
-    // connection: pgConnection,
-    connection: {
-      database: 'auth',
-      user: 'postgres',
-      password: 'secretpassword',
-      port: 5433
-    },
+    connection: pgConnection,
+    // connection: {
+    //   database: 'auth',
+    //   user: 'postgres',
+    //   password: 'secretpassword',
+    //   port: 5433
+    // },
     pool: {
       min: 2,
       max: 10,
