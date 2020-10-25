646d74c0ff876c2a28991144014e0dabf052c2db
maribelcuales <maribelcuales@gmail.com>
2020-10-25 23:48:18 +0800

    Add post method to router which calls the isValid function.

--- users/users-router.js
+++ users/users-router.js
@@ -4,6 +4,19 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const { isValid } = require("../users/users-service.js");
 
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+  if(isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;    
+  } else {
+    res
+      .status(400)
+      .json({
+        message: "please provide username and password and the password should be alphanumeric",
+      });
+  }
+});
+
 router.get("/", (req, res) => {
   Users.find()
     .then(users => {
