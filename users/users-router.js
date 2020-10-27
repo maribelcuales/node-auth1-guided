e8b8350c353dc0fdb348eb4a67c476e11465a816
maribelcuales <maribelcuales@gmail.com>
2020-10-27 20:44:03 +0800

    Hash the password.

--- users/users-router.js
+++ users/users-router.js
@@ -7,7 +7,10 @@ const { isValid } = require("../users/users-service.js");
 router.post("/register", (req, res) => {
   const credentials = req.body;
   if(isValid(credentials)) {
-    const rounds = process.env.BCRYPT_ROUNDS || 8;    
+    const rounds = process.env.BCRYPT_ROUNDS || 8;  
+    // hash the password 
+    const hash = bcryptjs.hashSync(credentials.password, rounds); 
+    credentials.password = hash;   
   } else {
     res
       .status(400)
