9bd243dfe8560a9ad7f1d7ca4e328e74a623389c
maribelcuales <maribelcuales@gmail.com>
2020-10-30 22:36:42 +0800

    Create function restricted.

--- users/users-router.js
+++ users/users-router.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
+function restricted(req, res, next) {
+  if(req.session && req.session.loggedIn) {
+    next();
+  } else {
+    res.status(401).json({ you: "cannot pass!" });
+  }
+}
+
 router.get("/", (req, res) => {
   Users.find()
     .then(users => {
