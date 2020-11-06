e5604e89bbd51530d46d68999afc11769dc52eda
maribelcuales <maribelcuales@gmail.com>
2020-11-06 20:23:01 +0800

    Added post method to route login.

--- auth/router.js
+++ auth/router.js
@@ -34,3 +34,30 @@ router.post("/register", (req, res) => {
       });
   }
 });
+
+router.post("/login", (req, res) => {
+  const { username, password} = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          // we can save information about the client inside the session (req.session)
+          req.session.loggedIn = true;
+          req.session.user = user;
+
+          res.status(200).json({ message: "Welcome to our API" });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
+})
