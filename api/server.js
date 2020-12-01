96d4e2cd8e1150010c70141694fbb9ef48c04b9d
maribelcuales <maribelcuales@gmail.com>
2020-12-01 21:00:10 +0800

    Create sessionConfig function to handle cookies.

--- api/server.js
+++ api/server.js
@@ -8,6 +8,18 @@ const authRouter = require("../auth/router.js");
 
 const server = express();
 
+const sessionConfig = {
+  cookie: {
+    maxAge: 1000 * 60 * 60, // one hour in milliseconds 
+    secure: process.env.SECURE_COOKIE || false, // send the cookie only over https, true in production 
+    httpOnly: true, // true means client JS cannot access the cookie
+  },
+  resave: false,
+  saveUninitialized: process.env.USER_ALLOWED_COOKIES || true,
+  name: "monster",
+  secret: process.env.COOKIE_SECRET || "keepitsecret, keepitsafe!",
+}; 
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
